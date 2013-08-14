e2499fb6d96c488404254bd09072e2f1084772af
tinitiate <tinitiate@gmail.com>
2013-08-14 16:58:05 -0400

    Create JavaPrint.java

---
+++ JavaPrint.java
@@ -0,0 +1,37 @@
+package JavaBasics;
+
+//*==============================================
+//* TINITIATE.COM
+//* --> T..O..P..I..C
+//* Printing text in JAVA
+//* Adding Code Comments
+//* Single Line and Multi line comments
+
+
+// Code Comments
+/*
+   Multiline Comments
+   Line 1
+   Line 2
+*/
+
+/*  
+	This Program Prints a message: "The Print Statement"
+*/
+
+public class JavaPrint 
+{
+	   public static void main(String[] args)
+	   {
+		   System.out.println("System.out.println --> LN Prints this text and ADDs a line break");
+		   System.out.println("System.out.print --> NO-LN Prints this text and DOES NOT ADD a line break");
+		   System.out.print("Message 1 in this line");
+		   System.out.print("Message 2 in the  same line");
+		   System.out.print("\n");
+		   System.out.println("Printing to console in Java: Hello Text !");
+		   System.out.println("Single line Comments are placed using: //");
+		   System.out.println("Multiline Comments are placed using: /* */");
+		   System.out.println("/* My Comment Line 1");
+		   System.out.println(" My Comment Line 2 */");
+	   }
+}
